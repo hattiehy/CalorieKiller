b05b4a5fbb1d3bdf90a2d4de0493d314e0b2b07f
hlii0132 <hlii0132@student.monash.edu>
2021-09-10 04:12:17 +1000

    calculator finished

--- Calendula/src/main/java/es/usc/citius/servando/calendula/activities/SelectPicActivity.java
+++ Calendula/src/main/java/es/usc/citius/servando/calendula/activities/SelectPicActivity.java
@@ -3,33 +3,32 @@ package es.usc.citius.servando.calendula.activities;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
-import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.codingending.popuplayout.PopupLayout;
 
+import org.apache.commons.lang3.StringUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.File;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -50,6 +49,10 @@ public class SelectPicActivity extends AppCompatActivity {
     List<Map<String,Object>> mFoodData;
     private RecyclerView mRecyclerView;
     private ItemClickAdapter adapter;
+    private double quantityNum = 0;
+    private double servingQuantity = 0;
+    private int caloriePerkg = 0;
+    private int calorie = 0;
 
     private static final int PHOTO_PHOTOALBUM = 0;
     private static String MY_TOKEN = null;
@@ -77,8 +80,60 @@ public class SelectPicActivity extends AppCompatActivity {
         });
 
         mFoodData = JSONUtil.getInitalListData();
+    }
+
+    public void selectServing(final List<String> list) {
+        View parent=View.inflate(SelectPicActivity.this,R.layout.layout_food_servings,null);
+        ListView listView=parent.findViewById(R.id.listview_bottom);
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,list);
+
+        final PopupLayout popupLayout=PopupLayout.init(SelectPicActivity.this,parent);
+        popupLayout.setHeight(350,true);
+        popupLayout.setUseRadius(true);
+        //popupLayout.setWidth(320,true);//手动设置弹出布局的宽度
+        popupLayout.show();//默认从底部弹出
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String raw = list.get(i);
+                String strQuantity = raw.split(", ")[1];
+                servingQuantity = Double.parseDouble(strQuantity.split(" ")[0]);
+                popupLayout.dismiss();
+                enterQuantity();
+            }
+        });
+
+    }
 
 
+
+    public void enterQuantity() {
+        View parent=View.inflate(SelectPicActivity.this,R.layout.layout_buttom,null);
+        final PopupLayout popupLayout=PopupLayout.init(SelectPicActivity.this,parent);
+        popupLayout.setHeight(350,true);
+        final EditText etQuantity = parent.findViewById(R.id.et_serving_num);
+        parent.findViewById(R.id.done_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (StringUtils.isBlank(etQuantity.getText())){
+                    Toast.makeText(SelectPicActivity.this, "Please enter the number of servings", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                quantityNum = Double.parseDouble(etQuantity.getText().toString());
+                popupLayout.dismiss();
+                calorie = calculateCalorie();
+            }
+        });
+        popupLayout.setUseRadius(true);
+        popupLayout.show(PopupLayout.POSITION_BOTTOM);
+    }
+
+    public int calculateCalorie() {
+        double qua = servingQuantity * quantityNum;
+        int calorie = (int) (qua * caloriePerkg);
+        return calorie;
     }
 
     public void setmRecyclerView() {
@@ -87,9 +142,30 @@ public class SelectPicActivity extends AppCompatActivity {
         adapter.setOnItemClickListener(new BaseQuickAdapter.OnItemClickListener() {
             @Override
             public void onItemClick(BaseQuickAdapter adapter, View view, int position) {
-                Toast.makeText(SelectPicActivity.this, "onItemClick" + position, Toast.LENGTH_SHORT).show();
+//                Toast.makeText(SelectPicActivity.this, "onItemClick" + position, Toast.LENGTH_SHORT).show();
+                String s = (String) mFoodData.get(position).get("calorie");
+                caloriePerkg = Integer.parseInt(s);
+                try {
+                    List<String> servingList = setServingList(position);
+                    selectServing(servingList);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
             }
         });
+    }
+
+    public List<String> setServingList(int position) throws JSONException {
+        JSONArray servingArr = (JSONArray) mFoodData.get(position).get("servingSizes");
+        List<String> servingList = new ArrayList<>();
+        for (int i = 0 ; i < servingArr.length(); i++) {
+            JSONObject serving =servingArr.getJSONObject(i);
+            String serDetail = serving.getString("unit") + ", " + serving.getString("servingWeight") + " kg";
+            servingList.add(serDetail);
+        }
+        return servingList;
     }
 
     private void initAdapter() {
